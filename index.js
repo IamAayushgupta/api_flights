a46acb2dc91f4bb49bced9c3d15d3f8d3b22674a
IamAayushgupta <ayush00768@gmail.com>
2025-10-16 09:57:44 +0530

    cors error

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 // index.js
 const express = require('express');
+const cors = require('cors');
 const path = require('path');
+
 
 const app = express();
 const PORT = process.env.PORT || 8000;
 
 // ----- Middleware -----
+app.use(cors());
 app.use(express.json()); // parse JSON bodies
 app.use(express.urlencoded({ extended: true }));
 
